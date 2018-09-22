7fa3cdd0d2085e402a50aa8f72876910f80190e3
axruff <39738572+axruff@users.noreply.github.com>
2018-09-22 10:43:00 +0200

    UPD

--- correlation_2d.cu
+++ correlation_2d.cu
@@ -11,12 +11,6 @@
 * This program is copyrighted by the author and Institute for Photon Science and Synchrotron Radiation,
 * Karlsruhe Institute of Technology, Karlsruhe, Germany;
 *
-* The current implemetation contains the following licenses:
-*
-* 1. TinyXml package:
-*      Original code (2.0 and earlier )copyright (c) 2000-2006 Lee Thomason (www.grinninglizard.com). <www.sourceforge.net/projects/tinyxml>.
-*      See src/utils/tinyxml.h for details.
-*
 */
 
 #include <device_launch_parameters.h>
